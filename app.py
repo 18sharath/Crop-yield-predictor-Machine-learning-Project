5c7ea688bd4dc435dfebbf4f72fe3889023e8942
sharath <sharath.t9480@gmail.com>
2024-12-29 23:47:58 +0530

    thinking to do price model

--- app.py
+++ app.py
@@ -20,7 +20,9 @@ import pickle
 import sklearn
 print(sklearn.__version__)
 #loading models
-dtr = pickle.load(open('dtr.pkl','rb'))
+
+
+best_model = pickle.load(open('best_model.pkl','rb'))
 preprocessor = pickle.load(open('preprocessor.pkl','rb'))
 
 #flask app
@@ -35,14 +37,14 @@ def predict():
         Year = request.form['Year']
         average_rain_fall_mm_per_year = request.form['average_rain_fall_mm_per_year']
         pesticides_tonnes = request.form['pesticides_tonnes']
-        avg_temp = request.form['avg_temp']
+        avg_temp = float(request.form['avg_temp'])
         Area = request.form['Area']
         Item  = request.form['Item']
-
+        
         features = np.array([[Area,Item,Year,average_rain_fall_mm_per_year,pesticides_tonnes,avg_temp]],dtype=object)
         transformed_features = preprocessor.transform(features)
         # prediction = dtr.predict(transformed_features).reshape(1,-1)
-        predicted_value= dtr.predict(transformed_features).reshape(1,-1)
+        predicted_value= best_model.predict(transformed_features).reshape(1,-1)
 
         return render_template('index.html',prediction = predicted_value)
 
