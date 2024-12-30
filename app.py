e24c9971f0b934ecd477d39fb40267c649a5955c
sharath <sharath.t9480@gmail.com>
2024-12-30 17:43:45 +0530

    sujan did something

--- app.py
+++ app.py
@@ -1,55 +1,51 @@
-# from flask import Flask, request
-# import numpy as np
-# import pandas as pd
-# import pickle
-
-# # creating app flask
-# app=Flask(__name__)
-
-#  if __name__=='__main__':
-#     app.run(debug=True)
-
-
-
-
-
-
-from flask import Flask,request, render_template
 import numpy as np
 import pickle
-import sklearn
-print(sklearn.__version__)
-#loading models
+from flask import Flask, request, render_template
 
+# Load models
+best_model_yield = pickle.load(open('yield_model.pkl', 'rb'))
+best_model_price = pickle.load(open('price_model.pkl', 'rb'))
+preprocessor = pickle.load(open('preprocessor.pkl', 'rb'))
 
-best_model = pickle.load(open('best_model.pkl','rb'))
-preprocessor = pickle.load(open('preprocessor.pkl','rb'))
-
-#flask app
+# Flask app
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
-@app.route("/predict",methods=['POST'])
+
+@app.route('/predict', methods=['POST'])
 def predict():
     if request.method == 'POST':
-        Year = request.form['Year']
-        average_rain_fall_mm_per_year = request.form['average_rain_fall_mm_per_year']
-        pesticides_tonnes = request.form['pesticides_tonnes']
+        Year = int(request.form['Year'])
+        average_rain_fall_mm_per_year = float(request.form['average_rain_fall_mm_per_year'])
+        pesticides_tonnes = float(request.form['pesticides_tonnes'])
         avg_temp = float(request.form['avg_temp'])
         Area = request.form['Area']
-        Item  = request.form['Item']
+        Item = request.form['Item']
         
-        features = np.array([[Area,Item,Year,average_rain_fall_mm_per_year,pesticides_tonnes,avg_temp]],dtype=object)
+        features = np.array([[Area, Item, Year, average_rain_fall_mm_per_year, pesticides_tonnes, avg_temp]], dtype=object)
         transformed_features = preprocessor.transform(features)
-        # prediction = dtr.predict(transformed_features).reshape(1,-1)
-        predicted_value= best_model.predict(transformed_features).reshape(1,-1)
+        predicted_value = best_model_yield.predict(transformed_features).reshape(1, -1)
 
-        return render_template('index.html',prediction = predicted_value)
+        return render_template('index.html', prediction=predicted_value)
 
-@app.route('/price',methods=['POST'])
-def price()
+@app.route('/price', methods=['GET', 'POST'])
+def price():
+    if request.method == 'POST':
+        Year = int(request.form['Year'])
+        average_rain_fall_mm_per_year = float(request.form['average_rain_fall_mm_per_year'])
+        pesticides_tonnes = float(request.form['pesticides_tonnes'])
+        avg_temp = float(request.form['avg_temp'])
+        Area = request.form['Area']
+        Item = request.form['Item']
+        
+        features = np.array([[Area, Item, Year, average_rain_fall_mm_per_year, pesticides_tonnes, avg_temp]], dtype=object)
+        transformed_features = preprocessor.transform(features)
+        predicted_price = best_model_price.predict(transformed_features).reshape(1, -1)
 
-if __name__=="__main__":
+        return render_template('price.html', price_prediction=predicted_price)
+    return render_template('price.html')
+
+if __name__ == "__main__":
     app.run(debug=True)
